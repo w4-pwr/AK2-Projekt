2951d15325c58da9e9bc17a3fbfd78757afee347
Jakub Pomykała <evelan3094@gmail.com>
2016-04-12 10:45:05 +0200

    update

--- lcd.c
+++ lcd.c
@@ -84,17 +84,27 @@ void initGPS(){
 
 // ------------------------ LCD 
 //inicjalizacja wyswietlacza
-void initLCD(){
+int initLCD(){
+	int lcd;
+	if (lcd = lcdInit (2, 16,4, LCD_RS, LCD_E ,LCD_D4 , LCD_D5, LCD_D6,LCD_D7,0,0,0,0)){
+        printf ("Nie udana inicjalizacja LCD! \n");
+            return -1;
+    }
+    return lcd;
 }
 
 //wyswietlenie napisu oczekiwania
-void displayWaiting(){
-	//lcdPuts(lcd, "Waiting for signal...");
+void displayWaiting(int lcd){
+	lcdClear(lcd); 
+	lcdPosition(lcd,0,0);
+	lcdPuts(lcd, "Czekanie na sygnal");   
 }
 
 //wyswietlanie wspolrzednych na wyswieltaczu
-void displayCoords(float lat, float lng){
+void displayCoords(int lcd, float lat, float lng){
+	lcdClear(lcd); 
 	lcdPuts(lcd, "Lat: ");
+	lcdPosition(lcd,1,0);   
 	lcdPuts(lcd, "Lng: ");
 }
 
@@ -115,17 +125,18 @@ void saveCoords(float lat, float lng){
  
 int main()
 {
-    int lcd;                //Handle for LCD
-    wiringPiSetup();        //Initialise WiringPi
- 
-    //Initialise LCD(int rows, int cols, int bits, int rs, int enable, int d0, int d1, int d2, int d3, int d4, int d5, int d6, int d7)
-    if (lcd = lcdInit (2, 16,4, LCD_RS, LCD_E ,LCD_D4 , LCD_D5, LCD_D6,LCD_D7,0,0,0,0)){
-            printf ("lcdInit failed! \n");
-            return -1 ;
-    }
- 
-    lcdPosition(lcd,0,0);           //Position cursor on the first line in the first column
-    lcdPuts(lcd, "Character LCD");  //Print the text on the LCD at the current cursor postion
-    getchar();                      //Wait for key press
-    lcdClear(lcd);                  //Clear the display
+  	//inicjalizacja 
+    wiringPiSetup();
+ 	int lcd = initLCD();
+ 	int gps = initGPS();
+
+ 	int hasSignal = waitingForSingal();
+ 	while(hasSignal){
+ 		float lat = getLatitude();
+ 		float lng = getLongitude();
+ 		displayCoords(lcd, lat, lng);
+ 		saveCoords(lat, lng);
+ 	}
+  
+    getchar();
 }
